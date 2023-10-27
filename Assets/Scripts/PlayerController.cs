3e5daf77ade8386ffe8668502c97684f7be15984
TF2026 <tfleitas26@sandomenico.org>
2023-10-27 11:59:52 -0700

    10/27/23

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       void OnJump()
+       {
+            OnJump(input: InputValue);
+       }
+        void OnCollisionEnter;
+            {
+            (collision: Collision);
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
